9ffc57f5c0a454bcda8aedb2b1176a6c5f957de2
lahaymd <lahaymd@gmail.com>
2017-05-11 13:50:10 -0500

    added d3

--- public/javascripts/d3.js
+++ public/javascripts/d3.js
@@ -1,57 +1,82 @@
-window.addEventListener('DOMContentLoaded', function(){
-	console.log('dom loaded');
-	// alert(1)
+// window.addEventListener('DOMContentLoaded', function(){
+// 	console.log('dom loaded');
+// 	// alert(1)
 
-	// d3.select("body").append("p").text("new paragraph!");
-	setTimeout(function(){
+// 	// d3.select("body").append("p").text("new paragraph!");
+// 	setTimeout(function(){
 
 
-		var jsonCircles = [
-   { "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
-   { "x_axis": 70, "y_axis": 70, "radius": 20, "color" : "purple"},
-   { "x_axis": 110, "y_axis": 100, "radius": 20, "color" : "red"}];
+// 		var jsonCircles = [
+//    { "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
+//    { "x_axis": 70, "y_axis": 70, "radius": 20, "color" : "purple"},
+//    { "x_axis": 110, "y_axis": 100, "radius": 20, "color" : "red"}];
  
- var svgContainer = d3.select("body").append("svg")
-                                     .attr("width", 200)
-                                     .attr("height", 200);
+//  var svgContainer = d3.select("body").append("svg")
+//                                      .attr("width", 200)
+//                                      .attr("height", 200);
  
-var circles = svgContainer.selectAll("circle")
-                          .data(jsonCircles)
-                          .enter()
-                          .append("circle");
+// var circles = svgContainer.selectAll("circle")
+//                           .data(jsonCircles)
+//                           .enter()
+//                           .append("circle");
 
-// var scale = d3.scale.linear();
+// // var scale = d3.scale.linear();
 
-var circleAttributes = circles
-                       .attr("cx", function (d) { return d.x_axis; })
-                       .attr("cy", function (d) { return d.y_axis; })
-                       .attr("r", function (d) { return d.radius; })
-                       .style("fill", function(d) { return d.color; })
-						.transition()
-					    .duration(750)
-					    .delay(function(d, i) { return i * 10; })
-					    .attr("r", function(d) { return d.radius * 2 });
-
-
-
-// Update…
-var p = d3.select("body")
-  .selectAll("p")
-  .data([4, 8, 15, 16, 23, 42])
-    // .text(function(d) { return d; });
-
-// Enter…
-p.enter().append("p")
-    .text(function(d) { return 'entering' + d; });
-
-// Exit…
-p.exit().remove();
-	}, 2000)
-	
-})
+// var circleAttributes = circles
+//                        .attr("cx", function (d) { return d.x_axis; })
+//                        .attr("cy", function (d) { return d.y_axis; })
+//                        .attr("r", function (d) { return d.radius; })
+//                        .style("fill", function(d) { return d.color; })
+// 						.transition()
+// 					    .duration(750)
+// 					    .delay(function(d, i) { return i * 10; })
+// 					    .attr("r", function(d) { return d.radius * 2 });
 
 
 
-// 	function() {
-//   return "hsl(" + Math.random() * 360 + ",100%,50%)";
-// });
+// // Update…
+// var p = d3.select("body")
+//   .selectAll("p")
+//   .data([4, 8, 15, 16, 23, 42])
+//     // .text(function(d) { return d; });
+
+// // Enter…
+// p.enter().append("p")
+//     .text(function(d) { return 'entering' + d; });
+
+// // Exit…
+// p.exit().remove();
+// // var data = [11,22,33];
+// // var inc = 0;
+
+// //         var svgContainer = d3.select("body").append("svg")
+// //                                      .attr("width", 200)
+// //                                      .attr("height", 200);
+ 
+// // var circles = svgContainer.selectAll("circle")
+// //                           .data(data)
+// //                           .enter()
+// //                           .append("circle")
+// //                           .attr("cx", function (d) { return d; })
+// //                        .attr("cy", function (d) { return d; })
+// //                        .attr("r", function (d) { return 10; })
+// //                        .style("fill", function(d) { return 'blue'; });
+
+// //   document.addEventListener('click', function(){
+// //     data.push(inc)
+// //     inc+=30;
+// //     alert(inc)
+// //     alert(data)
+//   // });
+  
+//   }, 2000)
+// })
+
+
+
+
+
+
+// // 	function() {
+// //   return "hsl(" + Math.random() * 360 + ",100%,50%)";
+// // });
---
+++ public/javascripts/global.js
@@ -0,0 +1,26 @@
+setTimeout(function(){
+
+
+
+var ab = document.createElementNS('http://www.w3.org/2000/svg','svg');
+ab.setAttribute('height', '500');
+ab.setAttribute('width', '500');
+var cd = document.createElementNS('http://www.w3.org/2000/svg','path');
+document.body.appendChild(ab);
+ab.appendChild(cd);
+cd.setAttribute('d', 'M150 0 L75 200 L225 200');
+cd.setAttribute('stroke', 'yellow');
+	var p= [
+function repeatOften() {
+  // Do whatever
+
+
+
+
+
+  requestAnimationFrame(repeatOften);
+}
+requestAnimationFrame(repeatOften);
+
+
+},3000)
