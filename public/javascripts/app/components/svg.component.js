0bb92831b007eafc86c58006d34c95b0903085f6
lahaymd <lahaymd@gmail.com>
2017-08-01 20:39:55 -0500

    added canvas and svg

--- public/javascripts/app/components/svg.component.js
+++ public/javascripts/app/components/svg.component.js
@@ -2,7 +2,14 @@
 
 	angular.module('myApp').
 		component('svgComponent', {
-			templateUrl: '/partials/comic-svg'
+			templateUrl: '/partials/comic-svg',
+			controller: svgController
 		})
+
+		function svgController(){
+			var g = angular.element(document.querySelector('#pow'));
+			console.log('g',g)
+			// g.attr('fill', 'url(svg/#pattern)')
+		}
 
 })()
