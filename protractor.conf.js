b199441a6f2df08015bdaacc895cb53003c0e31c
lahaymd <lahaymd@gmail.com>
2017-09-09 17:30:59 -0500

    bash

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
 // chromeOnly: true,
-directConnect: true,
+// directConnect: true,
 	specs: ['home.spec.js'],
 	 params: {
         client: 'http://localhost:3000/'
@@ -9,6 +9,6 @@ directConnect: true,
   //   browserName: 'chrome'
   // }
   // ,
-  allScriptsTimeout: 60000
-  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  allScriptsTimeout: 60000,
+  seleniumAddress: 'http://localhost:4444/wd/hub'
 };
