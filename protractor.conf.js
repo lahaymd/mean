d15f578559247a1349238727ac69e95ce1dd4dc9
lahaymd <lahaymd@gmail.com>
2017-09-10 05:26:37 -0500

    this should work

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
 chromeOnly: true,
-directConnect: true,
+// directConnect: true,
 	specs: ['home.spec.js'],
 	 params: {
         client: 'http://localhost:3000/'
@@ -8,6 +8,7 @@ directConnect: true,
 	 // capabilities: {
   //   browserName: 'chrome'
   // }
+  framework: 'jasmine2',
   // ,
   allScriptsTimeout: 60000
   // seleniumAddress: 'http://localhost:4444/wd/hub'
