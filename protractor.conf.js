d4d4e9aa2a61894badfd3aa7ccae211e59ac4c1e
lahaymd <lahaymd@gmail.com>
2017-09-11 22:12:31 -0500

    green

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,4 +12,17 @@ chromeOnly: true,
   // ,
   allScriptsTimeout: 60000
   // seleniumAddress: 'http://localhost:4444/wd/hub'
+
 };
+
+
+
+if (process.env.TRAVIS) {
+  config.sauceUser = process.env.SAUCE_USERNAME;
+  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+  config.capabilities = {
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER
+  };
+}
