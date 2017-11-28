d5a7af67b5f42d0cf3f38555ab2e6ec499199381
lahaymd <lahaymd@gmail.com>
2017-11-27 20:26:24 -0600

    updated protractor tests

--- home.spec.js
+++ home.spec.js
@@ -1,19 +1,22 @@
 //protractor tests
     // browser.driver.manage().window().maximize();
-var width = 1000;
-var height = 600;
-browser.driver.manage().window().setSize(width, height);
-describe('angularjs homepage todo list', function() {
-    // console.log(browser)
-    browser.get(browser.params.client);
-  beforeEach(function(){
+
+
     
-  })
-  it('should add a todo', function() {
-    expect(browser.getTitle()).toEqual('mikelahay.com');
-  });
-  it('should go to resume', function(){
-    element(by.id('contactMe')).click();    
-    expect(browser.getCurrentUrl()).toEqual(browser.params.client + 'hireme');
-  })
-});
+// var width = 1000;
+// var height = 600;
+// browser.driver.manage().window().setSize(width, height);
+// describe('angularjs homepage todo list', function() {
+//     // console.log(browser)
+//     browser.get(browser.params.client);
+//   beforeEach(function(){
+    
+//   })
+//   it('should add a todo', function() {
+//     expect(browser.getTitle()).toEqual('mikelahay.com');
+//   });
+//   it('should go to resume', function(){
+//     element(by.id('contactMe')).click();    
+//     expect(browser.getCurrentUrl()).toEqual(browser.params.client + 'hireme');
+//   })
+// });
